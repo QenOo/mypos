2703fee386d486a6a0c1ae088f9c8ef9f9f80361
QeNo <moodiiy.98@gmail.com>
2021-09-13 13:18:37 +0200

    initial project

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.dashboard.app')
+@section('content')
+
+    <div class="content-wrapper qeno">
+        <section class="content-header">
+            <h1>
+                this is a dashboard
+            </h1>
+
+        </section>
+
+        <section class="content">
+            <h1>ٍ]بسيبسي</h1>
+        </section>
+    </div>
+
+@endsection
---
+++ routes/dashboard/web.php
@@ -0,0 +1,18 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Dashboard\DashboardController;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+
+
+Route::group(['prefix' => LaravelLocalization::setLocale(),'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]],
+    function () {
+
+        Route::prefix('dashboard')->name('dashboard.')->group(function () {
+
+            Route::get('index', [DashboardController::class, 'index'])->name('index');
+
+        });
+
+});
+
+
