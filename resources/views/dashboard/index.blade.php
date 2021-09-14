0d1b9a2320da15018b135ec338d38ac53b2ccdef
QeNo <moodiiy.98@gmail.com>
2021-09-14 10:58:48 +0200

    add promisse

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -4,13 +4,16 @@
     <div class="content-wrapper qeno">
         <section class="content-header">
             <h1>
-                this is a dashboard
+                {{ __('site.dashboard') }}
             </h1>
 
+            <ol class="breadcrumb">
+                <li class="active"><i class="fa fa-dashboard"></i> @lang('site.dashboard')</li>
+            </ol>
         </section>
 
         <section class="content">
-            <h1>ٍ]بسيبسي</h1>
+            <h1>ٍالرئيسية</h1>
         </section>
     </div>
 
--- routes/dashboard/web.php
+++ routes/dashboard/web.php
@@ -1,4 +1,6 @@
 <?php
+
+use App\Http\Controllers\Dashboard\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Dashboard\DashboardController;
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
@@ -10,8 +12,15 @@ Route::group(['prefix' => LaravelLocalization::setLocale(),'middleware' => [ 'lo
         Route::prefix('dashboard')->name('dashboard.')->group(function () {
 
             Route::get('index', [DashboardController::class, 'index'])->name('index');
+            Route::get('users', [UserController::class, 'index'])->name('users');
+
+//            Route::resource('users', UserController::class);
 
         });
+
+        // user route
+
+
 
 });
 
